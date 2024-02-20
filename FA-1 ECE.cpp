d75a2edae05f766a19108facd3b196246be80929
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-02-20 16:44:10 +0530

    Create FA-1 ECE.cpp

---
+++ FA-1 ECE.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int Maximum(int x, int y)
+{
+	// Write your code here.
+}
+void Swap(int &x, int &y)
+{
+	// Write your code here.
+}
+int main()
+{
+	int test, a, b, r;
+	cin >> test;
+	cin >> a >> b;
+	switch (test)
+	{
+	case 1:
+		r = Maximum(a, b);
+		cout << r;
+		break;
+	case 2:
+		Swap(a, b);
+		cout << a << " " << b;
+		break;
+	default:
+		cout << "Invalid test option";
+	}
+	return 0;
+}
