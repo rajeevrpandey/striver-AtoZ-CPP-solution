25241650f63d6948e9413f12f119449b401f9b6c
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-02-20 16:35:48 +0530

    Create Find Character Case.cpp

---
+++ Find Character Case.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+ 
+
+int main() {
+
+    char c;
+
+    cin>>c;
+
+    if (isupper(c)){
+
+        cout<<"1";
+
+    }
+
+    else if (islower(c)){
+
+        cout<<"0";
+
+    }
+
+    else{
+
+        cout<<"-1";
+
+    }
+
+      
+
+}
