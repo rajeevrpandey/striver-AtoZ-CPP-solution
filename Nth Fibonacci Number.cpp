7ae1dbded1344e30bd1158b54ca884e67b7ab88b
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-02-20 16:41:58 +0530

    Create Nth Fibonacci Number.cpp

---
+++ Nth Fibonacci Number.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int fib(int n) {
+  if (n == 1 || n == 2) {
+    return 1;
+  }
+  return (fib(n - 1) + fib(n - 2));
+}
+
+
+int main() {
+  /*
+   *  Write your code here.
+   *  Read input as specified in the question.
+   *  Print output as specified in the question.
+   */
+  int n;
+  cin >> n;
+
+
+  cout << fib(n);
+}
